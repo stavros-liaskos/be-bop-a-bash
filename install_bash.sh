18b1ff1e0a9bc09ffe8f532202669cbdbd7ea455
Stavros Liaskos <st.liaskos@gmail.com>
2017-09-07 09:34:12 +0200

    update

--- install_bash.sh
+++ install_bash.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+git pull origin master;
+
 echo "Installing bash profile and git completion...";
 cp -v ./.bash_profile ./git-completion.bash ~
 source ~/.bash_profile

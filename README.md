e2677ade098607f42a62e785bf76c01191d54e52
Stalias <st.liaskos@gmail.com>
2024-06-03 15:51:12 +0200

    install plugins conditionally

--- README.md
+++ README.md
@@ -67,11 +67,6 @@ type -t foo
 
 You can always invoke cat after aliasing `ccat` by typing `\cat`.
 
-## Todo/Known Issues
-1. add `brew install jq`
-1. add conditional installers for plugins
-1. add shortcut for `add . && com 's' && pus`
-
 ## Extra
 - [Open PhpStorm from CLI](https://intellij-support.jetbrains.com/hc/en-us/community/posts/208396265-Can-I-open-a-directory-from-command-line-)  
 - [Run eslint --fix on each Save](https://www.jetbrains.com/help/phpstorm/2019.3/configuring-keyboard-and-mouse-shortcuts.html?utm_campaign=PS&utm_medium=link&utm_source=product&utm_content=2019.3#add-keyboard-shortcut)   
--- plugins-install.sh
+++ plugins-install.sh
@@ -1,64 +1,145 @@
 #!/usr/bin/env bash
 
-echo "Installing brew...";
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo "Installation complete";
+echo "Do you want to install brew? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        echo "Installation complete"
+fi
 
-echo "Installing ccat...";
-brew list ccat &>/dev/null || brew install ccat
-echo "Installation complete";
+echo "Do you want to install ccat? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        brew list ccat &>/dev/null || brew install ccat
+        echo "Installation complete"
+fi
 
-echo "Upgrading pip...";
-pip install --upgrade pip
-echo "Installation complete";
 
-echo "Installing wget...";
-brew list wget &>/dev/null || brew install wget
-echo "Installation complete";
+echo "Do you want to install pip? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        pip install --upgrade pip
+        echo "Installation complete"
+fi
 
-echo "Installing mkdocs...";
-pip install mkdocs
-echo "Installation complete";
 
-echo "Installing youtube-dl...";
-sudo -H pip install --upgrade youtube-dl
-echo "Installation complete";
+echo "Do you want to install wget? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        brew list wget &>/dev/null || brew install wget
+        echo "Installation complete"
+fi
 
-echo "Installing htop-osx...";
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null
-brew list htop-osx &>/dev/null || brew install htop-osx
-echo "Installation complete";
 
-echo "Installing ffmpeg...";
-brew list ffmpeg &>/dev/null || brew install ffmpeg $(brew options ffmpeg | grep -vE '\s' | grep -- '--with-' | tr '\n' ' ')
-echo "Installation complete";
+echo "Do you want to install mkdocs? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        pip install mkdocs
+        echo "Installation complete"
+fi
 
-echo "Installing yarn...";
-brew list yarn &>/dev/null || brew install yarn
-echo "Installation complete";
 
-echo "Installing vercel cli...";
-npm i -g vercel
-echo "Installation complete";
+echo "Do you want to install youtube-dl? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        sudo -H pip install --upgrade youtube-dl
+        echo "Installation complete"
+fi
 
-echo "Installing Image magick...";
-brew list imagemagick &>/dev/null || brew install imagemagick
-echo "Installation complete";
 
-echo "Installing Image httrack...";
-brew list httrack &>/dev/null || brew install httrack
-echo "Installation complete";
+echo "Do you want to install htop-osx? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        brew list htop-osx &>/dev/null || brew install htop-osx
+        echo "Installation complete"
+fi
 
-echo "Installing Image ncdu...";
-brew list ncdu &>/dev/null || brew install ncdu
-echo "Installation complete";
 
-echo "Installing nvm...";
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-echo "Installation complete";
+echo "Do you want to install ffmpeg? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        brew list ffmpeg &>/dev/null || brew install ffmpeg $(brew options ffmpeg | grep -vE '\s' | grep -- '--with-' | tr '\n' ' ')
+        echo "Installation complete"
+fi
 
-echo "Installing sqsmover...";
-brew list sqsmover &>/dev/null || brew install sqsmover
-echo "Installation complete";
+
+echo "Do you want to install yarn? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        brew list yarn &>/dev/null || brew install yarn
+        echo "Installation complete"
+fi
+
+
+echo "Do you want to install vercel? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        npm i -g vercel
+        echo "Installation complete"
+fi
+
+
+echo "Do you want to install Image magick? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        brew list imagemagick &>/dev/null || brew install imagemagick
+        echo "Installation complete"
+fi
+
+
+echo "Do you want to install jq? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        brew list jq &>/dev/null || brew install jq
+        echo "Installation complete"
+fi
+
+
+echo "Do you want to install httrack? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        brew list httrack &>/dev/null || brew install httrack
+        echo "Installation complete"
+fi
+
+
+echo "Do you want to install ncdu? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        brew list ncdu &>/dev/null || brew install ncdu
+        echo "Installation complete"
+fi
+
+
+echo "Do you want to install nvm? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+        echo "Installation complete"
+fi
+
+
+echo "Do you want to install sqsmover? [y,n]"
+read input
+if [[ $input == "Y" || $input == "y" ]]; then
+        echo "Installing..."
+        brew list sqsmover &>/dev/null || brew install sqsmover
+        echo "Installation complete"
+fi
 
 echo "Done!";
